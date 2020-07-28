f69fe9d3b2e1fc96be85aa2635282b067cafb447
Thomas Axelsson <thomasa88@gmail.com>
2020-07-28 11:30:50 +0200

    License

--- DirectName/DirectName.py
+++ DirectName/DirectName.py
@@ -1,5 +1,23 @@
 #Author-Thomas Axelsson
-#Description-Shows a naming dialog directly after creating a feature
+#Description-Shows a naming dialog directly after creating a feature.
+
+# This file is part of DirectName, a Fusion 360 add-in for naming
+# features directly after creation.
+#
+# Copyright (C) 2020  Thomas Axelsson
+#
+# DirectName is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DirectName is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DirectName.  If not, see <https://www.gnu.org/licenses/>.
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
