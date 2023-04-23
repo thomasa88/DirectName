ded2b6afa27d7a89d072d16970bd37e4d8a7556f
Thomas Axelsson <thomasa88@gmail.com>
2023-04-23 21:47:15 +0200

    Show version in menu, for easier troubleshooting

--- DirectName.py
+++ DirectName.py
@@ -414,7 +414,7 @@ def update_enable_button():
     
     # Note: Name must be updated for icon to change!
     # And the name must be set on the controlDefinition!
-    enable_cmd_def_.controlDefinition.name = f'Enable/Disable {NAME}'
+    enable_cmd_def_.controlDefinition.name = f'Enable/Disable {NAME} (v {manifest_["version"]})'
 
 def run(context):
     global app_
